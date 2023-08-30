4cc2e326355df9ee1754fdbe84c9e7156a00e069
Tella Abdulrasheed <rashtell2@gmail.com>
2023-08-30 07:39:06 +0100

    fixed variable expansion issue in if block

--- create-project.bat
+++ create-project.bat
@@ -23,13 +23,15 @@ set /p platform=Enter platform:
 set /p language=Enter language: 
 set /p organization=Enter organization: 
 
-set "workspace_path=C:\workspace"
-set "project_path=%workspace_path%\project\%project_name%"
-
 :: clear console
 cls
 
+set "workspace_path=C:\workspace"
+
+
 :: Handle project
+set "project_path=%workspace_path%\project\%project_name%"
+
 if not exist "%project_path%" (
     md "%project_path%"
 
@@ -38,59 +40,67 @@ if not exist "%project_path%" (
     echo Project directory already exists.
 )
 
+
 :: Handle platform
 set "platform_project_path=%workspace_path%\platform\%platform%\%project_name%"
+
 if not exist "%platform_project_path%" (
 
     set "platform_path=%workspace_path%\platform\%platform%"
-    if not exist "%platform_path%" (
-        md "%platform_path%"
+    if not exist "!platform_path!" (
+        md "!platform_path!"
     )
     
     mklink /D "%platform_project_path%" "%project_path%"
 
-    echo Platform symbolic link created.
+    echo Project linked to platform.
 ) else (
-    echo Platform symbolic link already exists.
+    echo Project already linked to platform.
 )
+
 
 :: Handle organization
 set "organization_project_path=%workspace_path%\organization\%organization%\%project_name%"
+set "organization_path=%workspace_path%\organization\%organization%"
+set "organization_project_path=%organization_path%\%project_name%"
+
 if not exist "%organization_project_path%" (
 
-    set "organization_path=%workspace_path%\organization\%organization%"
     if not exist "%organization_path%" (
         md "%organization_path%"
     )
 
     mklink /D "%organization_project_path%" "%project_path%"
 
-    echo Organization symbolic link created.
+    echo Project linked to organization.
 ) else (
-    echo Organization symbolic link already exists.
+    echo Project already linked to organization.
 )
+
 
 :: Handle language
 set "language_project_path=%workspace_path%\language\%language%\%project_name%"
+set "language_path=%workspace_path%\language\%language%"
+
 if not exist "%language_project_path%" (
 
-    set "language_path=%workspace_path%\language\%language%"
     if not exist "%language_path%" (
         md "%language_path%"
     )
 
     mklink /D "%language_project_path%" "%project_path%"
 
-    echo language symbolic link created.
+    echo Project linked to language.
 ) else (
-    echo language symbolic link already exists.
+    echo Project already linked to language.
 )
 
 if "%language%"=="php" (
     mklink /D "C:\xampp\htdocs\%project_name%" "%project_path%"
     mklink /D "C:\xampp7.4\htdocs\%project_name%" "%project_path%"
-    echo PHP symbolic links created.
+    echo Project linked to xampp.
 )
 
-echo Project setup completed.
+
+echo "Project setup completed."
 pause
