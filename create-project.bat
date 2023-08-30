3dab65ed33f9934f55336954eb4ded873f72b9eb
Tella Abdulrasheed <rashtell2@gmail.com>
2023-08-30 07:40:41 +0100

    handled javascript subsidiaries as language input

--- create-project.bat
+++ create-project.bat
@@ -1,6 +1,6 @@
 @echo off
 :: BatchGotAdmin code
-@echo off
+::#region
 setlocal
 if "%1"=="ELEV" (
     shift
@@ -15,21 +15,26 @@ del "%temp%\getadmin.vbs"
 exit /b
 
 :main
+::#endregion
+
 :: Actual script starts here
 setlocal EnableDelayedExpansion
 
+:: Inputs
+::#region
 set /p project_name=Enter project name: 
 set /p platform=Enter platform: 
 set /p language=Enter language: 
 set /p organization=Enter organization: 
+::#endregion
 
 :: clear console
 cls
 
 set "workspace_path=C:\workspace"
 
-
 :: Handle project
+::#region
 set "project_path=%workspace_path%\project\%project_name%"
 
 if not exist "%project_path%" (
@@ -39,9 +44,10 @@ if not exist "%project_path%" (
 ) else (
     echo Project directory already exists.
 )
-
+::#endregion
 
 :: Handle platform
+::#region
 set "platform_project_path=%workspace_path%\platform\%platform%\%project_name%"
 
 if not exist "%platform_project_path%" (
@@ -57,10 +63,10 @@ if not exist "%platform_project_path%" (
 ) else (
     echo Project already linked to platform.
 )
-
+::#endregion
 
 :: Handle organization
-set "organization_project_path=%workspace_path%\organization\%organization%\%project_name%"
+::#region
 set "organization_path=%workspace_path%\organization\%organization%"
 set "organization_project_path=%organization_path%\%project_name%"
 
@@ -76,11 +82,28 @@ if not exist "%organization_project_path%" (
 ) else (
     echo Project already linked to organization.
 )
-
+::#endregion
 
 :: Handle language
-set "language_project_path=%workspace_path%\language\%language%\%project_name%"
-set "language_path=%workspace_path%\language\%language%"
+::#region
+
+set "language_path=%workspace_path%\language"
+
+:: Appends javascript to language path if the inputted language is a javascript language
+for /d %%d in ("%language_path%\javascript\*") do (
+    set "folder_name=%%~nd"
+    if /i "!folder_name!"=="%language%" (
+        set "language_path=!language_path!\javascript\!language!"
+        goto :found
+    )
+)
+:found
+
+if "%language_path%" equ "%workspace_path%\language" (
+    set "language_path=!language_path!\!language!"
+)
+
+set "language_project_path=%language_path%\%project_name%"
 
 if not exist "%language_project_path%" (
 
@@ -100,7 +123,7 @@ if "%language%"=="php" (
     mklink /D "C:\xampp7.4\htdocs\%project_name%" "%project_path%"
     echo Project linked to xampp.
 )
-
+::#endregion
 
 echo "Project setup completed."
 pause
