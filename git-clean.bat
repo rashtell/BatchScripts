056ee56f4a2bc9688d3879a269705b1dc1d32904
Tella Abdulrasheed <rashtell2@gmail.com>
2026-04-08 15:40:59 +0100

    Add script to prune stale local branches
    Introduce a batch script to automate the removal of local branches that have been marked as "gone" by the remote tracking configuration. This simplifies repository maintenance by quickly cleaning up merged or deleted remote branches.

---
+++ git-clean.bat
@@ -0,0 +1,8 @@
+@echo off
+setlocal enabledelayedexpansion
+
+for /f "tokens=1" %%i in ('git branch -vv ^| findstr /C:"gone"') do (
+    set "branch=%%i"
+    if "!branch:~0,1!"=="*" set "branch=!branch:~2!"
+    git branch -d "!branch!"
+)
